a11e0a3c620c5af486936f6092b26438cee29acd
Ahmad Kayyali <kayyali18@gmail.com>
2018-07-20 16:04:50 -0600

    Add Local Storage mods

--- test.js
+++ test.js
@@ -2,7 +2,13 @@
 $('#title-input').on('keyup', btnState);
 $('#body-input').on('keyup', btnState);
 $('.save-btn').on('click', saveBtn);
-
+$.each(localStorage, function(key) {
+    debugger;
+    parseInt((JSON.parse(key)));
+    var cardData = JSON.parse(key);
+    console.log(cardData)
+    $( ".bottom-box" ).prepend(newCard(cardData));
+});
 
 onLoad();
 
@@ -40,24 +46,22 @@ function saveBtn (event) {
   event.preventDefault();
   var titleInput = ($('#title-input').val());
   var bodyInput = ($('#body-input').val());
-  newCard()
+  newCard(cardObject())
   localStoreCard();
 
 }
 
 
 
-function newCard () {
-    var idea = cardObject();
-    console.log(idea);
+function newCard (key) {
     var html =
-      `<div id="${idea.id}"class="card-container">
-        <h2 class="title-of-card">${idea.title}</h2>
+      `<div id="${key.id}"class="card-container">
+        <h2 class="title-of-card">${key.title}</h2>
         <button class="delete-button"></button>
-        <p class="body-of-card">${idea.body}</p>
+        <p class="body-of-card">${key.body}</p>
         <button class="upvote"></button>
         <button class="downvote"></button>
-        <p class="quality">quality: <span class="qualityVariable">${idea.quality}</span></p>
+        <p class="quality">quality: <span class="qualityVariable">${key.quality}</span></p>
         <hr>
       </div>`
     $('.bottom-box').prepend(html);
